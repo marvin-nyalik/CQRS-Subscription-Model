cc6a7c744e3642153389084db649a4cd1849dbb9
MARVIN <o.silverygold@gmail.com>
2026-01-29 06:49:27 +0300

    Init subscription service

---
+++ subscription/src/main/java/com/cqrs/subscription/service/SubscriptionService.java
@@ -0,0 +1,37 @@
+package com.cqrs.subscription.service;
+
+import com.cqrs.subscription.dto.CreateSubscriptionDTO;
+import com.cqrs.subscription.model.Subscription;
+import com.cqrs.subscription.model.SubscriptionStatus;
+import com.cqrs.subscription.repository.SubscriptionRepository;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class SubscriptionService {
+    private final SubscriptionRepository repository;
+
+    public SubscriptionService(SubscriptionRepository repository){
+        this.repository = repository;
+    }
+
+    public Subscription createSubscription(CreateSubscriptionDTO dto){
+        repository.findByUserIdAndStatus(dto.getUserId(), SubscriptionStatus.ACTIVE)
+                .ifPresent(s -> { throw new IllegalStateException("" +
+                        "User has an active subscription."); });
+        Subscription subscription =
+                Subscription.create(dto.getUserId(), dto.getPlanCode());
+        subscription.activate();
+        return repository.save(subscription);
+    }
+
+    public Subscription get(UUID id){
+        return repository.findById(id).orElseThrow();
+    }
+
+    public Subscription cancel(UUID id) {
+        Subscription sub = get(id);
+        sub.cancel();
+        return repository.save(sub);
+    }
+}
