c173bf7343b204d4df3d98867acb87a18a19a52e
MARVIN <o.silverygold@gmail.com>
2026-01-31 07:06:22 +0300

    Throw custom exception, mark cancel as transactional

--- subscription/src/main/java/com/cqrs/subscription/service/SubscriptionService.java
+++ subscription/src/main/java/com/cqrs/subscription/service/SubscriptionService.java
@@ -1,11 +1,12 @@
 package com.cqrs.subscription.service;
 
 import com.cqrs.subscription.dto.CreateSubscriptionDTO;
+import com.cqrs.subscription.exception.SubscriptionNotFoundException;
 import com.cqrs.subscription.model.Subscription;
 import com.cqrs.subscription.model.SubscriptionStatus;
 import com.cqrs.subscription.repository.SubscriptionRepository;
+import jakarta.transaction.Transactional;
 
-import java.time.Instant;
 import java.util.UUID;
 
 public class SubscriptionService {
@@ -17,8 +18,7 @@ public class SubscriptionService {
 
     public Subscription createSubscription(CreateSubscriptionDTO dto){
         repository.findByUserIdAndStatus(dto.getUserId(), SubscriptionStatus.ACTIVE)
-                .ifPresent(s -> { throw new IllegalStateException("" +
-                        "User has an active subscription."); });
+                .ifPresent(s -> { throw new IllegalStateException("User has an active subscription."); });
         Subscription subscription =
                 Subscription.create(dto.getUserId(), dto.getPlanCode());
         subscription.activate();
@@ -26,9 +26,11 @@ public class SubscriptionService {
     }
 
     public Subscription get(UUID id){
-        return repository.findById(id).orElseThrow();
+        return repository.findById(id).
+                orElseThrow(SubscriptionNotFoundException::new);
     }
 
+    @Transactional
     public Subscription cancel(UUID id) {
         Subscription sub = get(id);
         sub.cancel();
