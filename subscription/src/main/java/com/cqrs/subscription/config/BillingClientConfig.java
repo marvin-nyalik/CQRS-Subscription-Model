f8fa4c755084e23636dbcbe7cac4f22ae2a8075c
MARVIN <o.silverygold@gmail.com>
2026-02-17 06:11:38 +0300

    Revamp billingWebClient

--- subscription/src/main/java/com/cqrs/subscription/config/BillingClientConfig.java
+++ subscription/src/main/java/com/cqrs/subscription/config/BillingClientConfig.java
@@ -9,10 +9,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class BillingClientConfig {
 
     @Bean
-    @LoadBalanced
     WebClient billingWebClient(WebClient.Builder builder){
         return builder
-                .baseUrl("http://BILLING-SERVICE")
+                .baseUrl("http://billing-service")
                 .build();
     }
 }
